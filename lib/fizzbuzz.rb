256f0ed8b6a14a00ec4200cd7fc0db707b5c0fe9
Chris Mitchell <chrisjmit@gmail.com>
2017-04-03 22:02:49 +0100

    Test and edge cases passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  'fizz'
+  return "Enter a valid number please" if !number.is_a? Integer
+  return 'fizzbuzz' if number % 15 == 0
+  return 'buzz' if number % 5 == 0
+  return 'fizz' if number % 3 == 0
+  return number
 end
+
+#1.upto(100) {|i| puts fizzbuzz(i)}
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,36 @@
-require  './lib/fizzbuzz'
+require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+
+  it 'returns "not valid input" when passed !number' do
+    expect(fizzbuzz("akdsbf")).to eq 'Enter a valid number please'
+  end
+
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(5)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+
+  it 'returns 26 when passed 26' do
+    expect(fizzbuzz(26)).to eq 26
+  end
+
+  it 'returns "fizz" when passed -3' do
+    expect(fizzbuzz(-3)).to eq 'fizz'
+  end
+
+  it 'return -13 when passed -13' do
+    expect(fizzbuzz(-13)).to eq -13
   end
 end
